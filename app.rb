c70f18a7ed2c07ae115c05dcaface94bb154074b
Teddy Zetterlund <teddy.zetterlund@gmail.com>
2016-06-14 08:45:27 +0200

    Replace inline templates with views

--- app.rb
+++ app.rb
@@ -25,36 +25,3 @@ post '/customers/tags' do
 
   redirect to('/')
 end
-
-__END__
-
-@@ layout
-<!DOCTYPE html>
-<html lang="en">
-<meta charset="utf-8">
-
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,300,500">
-<link rel="stylesheet" href="/styles.css">
-
-<title>Intercom Web Interface</title>
-
-<h1>Intercom Web Interface</h1>
-<%= styled_flash %>
-<%= yield %>
-
-@@ index
-<form method="post" action="/customers/tags">
-  <label>
-    Customer e-mails <small>(separated by comma or line-breaks)</small>
-    <textarea class="text-input" name="emails"></textarea>
-  </label>
-
-  <label>
-    Name of tag <small>(to tag customers with)</small>
-    <input class="text-input" type="text" name="tag_name">
-  </label>
-
-  <div class="u-text-center">
-    <input class="button" type="submit" value="Tag customers">
-  </div>
-</form>
